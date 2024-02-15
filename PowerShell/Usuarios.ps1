39c9fbb0632be667c81219bd3620a3720c9a233d
dav200435 <davacas2005@gmail.com>
2024-02-15 19:02:43 +0100

    usuarios

---
+++ PowerShell/Usuarios.ps1
@@ -0,0 +1,24 @@
+﻿function crearUsuarios($archivo){
+    Get-Content $archivo | foreach{
+        $usuario = $_.Trim()
+        crearUser ($usuario)
+        echo("se ha creado $usuario")
+    }
+}
+
+function borrarUsuarios($archivo){
+    Get-Content $archivo | foreach{
+        $usuario = $_.Trim()
+        borrarUser ($usuario)
+        echo("se ha borrado el usuario: $usuario")
+    }
+}
+
+function crearUser($user){
+    New-LocalUser -Name $user -Password (ConvertTo-SecureString "Contraseña" -AsPlainText -Force) -FullName $user -Description "Descripción del Usuario"
+    Get-Localuser -Name $user | Add-LocalGroupMember -Group 'Usuarios'
+}
+
+function borrarUser($user){
+    Remove-LocalUser -Name $user
+}

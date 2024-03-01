703a161a3badff83bbaa640eff6b20510b5cfd56
dav200435 <davacas2005@gmail.com>
2024-03-01 20:39:34 +0100

    testing

---
+++ PowerShell/verProcesos.ps1
@@ -0,0 +1,10 @@
+﻿foreach($conex in @(netstat -ano))
+{
+    foreach($proceso in ps)
+    {
+        if($conex -match $proceso.Id -and $proceso.Id -ne 4 -and $proceso.Id -ne 0)
+        {
+            write-host $conex "-PROCESO->" $proceso.Name $proceso.Id
+        }
+    }
+}
